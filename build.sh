2a126dd3c5c28de9943f1d666d2decd18d236d82
菩提树下的杨过 <45684858+xiaoke321@users.noreply.github.com>
2024-08-21 10:49:03 +0800

    构建配置文件flash-oos_defconfig脚本和说明

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# 说明
+# CROSS_COMPILE构建工具：https://github.com/nathanchance/gcc-prebuilts/
+# CROSS_COMPILE构建工具分支clone：git clone -b aarch64-linaro-7.x --depth=1 https://github.com/nathanchance/gcc-prebuilts
+
 export ARCH=arm64
 export CROSS_COMPILE=/opt/gcc-prebuilts/bin/aarch64-linaro-linux-android-
 
